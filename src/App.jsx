c34000641855ab114f84707f04c749849b1111c2
Caio Adriano <caiobhadriano@gmail.com>
2024-01-04 21:35:06 -0300

    [Feat]: Getting option of user end initialiazing option of machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+function Header({ score }) {
+  return (
+    <>
+      <h1>Score: {score}</h1>
+    </>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  const options = ["Pedra", "Papel", "Tesoura"];
+
+  const [selectedPlayerOption,setSelectedPlayerOption] = useState("")
+  const [machineOption, setMachineOption] = useState("")
+
+  function getValue(e) {
+    setSelectedPlayerOption(e.target.value)
+    setMachineOption()
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header score={score} />
+
+      <div>Opção do player: {selectedPlayerOption}</div>
+      <div>Opção do maquina: {machineOption}</div>
+
+      {options.map((option, index) => {
+        return <button key={index} onClick={(e)=>{getValue(e)}} value={option}>{option}</button>;
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
